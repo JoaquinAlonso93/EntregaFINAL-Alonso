2c2325b2cbfce9caf7517e736b02a3a68825f20e
JoaquinAlonso93 <laboral.alonso93@gmail.com>
2024-07-13 16:07:03 -0300

    PreEntrega1

---
+++ script.js
@@ -0,0 +1,57 @@
+// variables globales
+const alimentoA = 100
+const alimentoB = 200
+const alimentoC = 1000
+
+let cantidad;
+let marcaAlimento;
+let resultado;
+
+//operacion multiplicar
+function multiplicar(valor1, valor2) {
+    const resultado = valor1 * valor2;
+    return resultado;
+}
+//paso 1: que nos diga por prompt la cantidad de kilos de alimento para mascota que quiere llevar, teniendo en cuenta que 0. cierra el ciclo.
+//paso 2: elija la marca de alimento que desee comprar, con A B y C.
+
+while (cantidad != 0) {
+    cantidad = parseInt(prompt("Ingresar cantidad de alimentos de comida vendido, recuerde que 0 cero, termina la carga"));
+
+    if (cantidad === 0) {
+        break;
+    }
+
+    marcaAlimento = prompt("Ingrese la marca de alimento que desee:\n\ -Si quiere alimento marca A ingrese = A \n\ -Si quiere alimento marca B ingrese = B \n\ -Si quiere alimento marca C ingrese = C");
+
+    switch (marcaAlimento) {
+        case "A":
+            resultado = multiplicar(alimentoA, cantidad);
+            alert("Debe abonar $ " + resultado + " por su compra de alimento marca A");
+            break;
+        case "B":
+            resultado = multiplicar(alimentoB, cantidad);
+            alert("Debe abonar $ " + resultado + " por su compra de alimento marca B");
+            break;
+        case "C":
+            resultado = multiplicar(alimentoC, cantidad);
+            alert("Debe abonar $ " + resultado + " por su compra de alimento marca C");
+            break;
+        default:
+            alert("Operacion Invalida");
+            break;
+    }
+
+    if (cantidad >= 10) {
+        valorEnvio = 0
+        alert("Su costo de envio es $" + valorEnvio);
+    }
+    else if (cantidad >= 5) {
+        valorEnvio = 100
+        alert("Su costo de envio es $" + valorEnvio);
+    }
+    else {
+        valorEnvio = 200
+        alert("Su costo de envio es $" + valorEnvio);
+    }
+}
